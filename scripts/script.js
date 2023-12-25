0e6188207e45c448dd6473a534be2f7d2202f000
Mostafa_Ahmed <mostafa.ahmed.mostafa726@gmail.com>
2023-12-25 20:10:07 +0200

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const getSecBtnsContainer = document.querySelectorAll('.controllers');
 const getSecBtns = document.querySelectorAll('.controller');
 const getKitchenGithub = document.getElementById('kitchen_restaurant_gitHub');
 const getKitchenLive = document.getElementById('kitchen_restaurant');
+const getStoreGithub = document.getElementById('Store_Management_Sytem_gitHub');
+const getStoreLive = document.getElementById('Store_Management_Sytem');
 
 getKitchenGithub.onclick = function() {
     getKitchenGithub.setAttribute('href', 'https://github.com/Mostafa-ElMonateh/ITI-project-HTML_CSS')
@@ -12,12 +14,12 @@ getKitchenLive.onclick = function() {
     getKitchenLive.setAttribute('href', 'https://mostafa-elmonateh.github.io/ITI-project-HTML_CSS/')
 }
 
-getKitchenGithub.onclick = function() {
-    getKitchenGithub.setAttribute('href', 'https://github.com/Mostafa-ElMonateh/Store-Management-System')
+getStoreGithub.onclick = function() {
+    getStoreGithub.setAttribute('href', 'https://github.com/Mostafa-ElMonateh/Store-Management-System')
 }
 
-getKitchenLive.onclick = function() {
-    getKitchenLive.setAttribute('href', 'https://mostafa-elmonateh.github.io/Store-Management-System/')
+getStoreLive.onclick = function() {
+    getStoreLive.setAttribute('href', 'https://mostafa-elmonateh.github.io/Store-Management-System/')
 }
 
 
